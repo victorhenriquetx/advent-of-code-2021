ae3f02e18e7e3b11ba9407fc73b63cea09692252
victorhenriquetx <victorhenrique60@hotmail.com>
2021-12-03 22:05:38 -0200

    add part one alternative solution

--- day_3/part_one.py
+++ day_3/part_one.py
@@ -1,7 +1,6 @@
 import sys
 
-
-def calc_power(report:list, size:int)->int:	
+def calc_power2(report:list, size:int)->int:	
 	count = [0]*size					
 	for num in report:
 		for i,bit in enumerate(num):
@@ -12,11 +11,25 @@ def calc_power(report:list, size:int)->int:
 	temp2 = temp ^ (2 ** (len(most_common) + 1)-1)
 	least_common = bin(temp2)[3:]
 	return int(most_common,2)* int(least_common,2)	 	
-        
-		
+
+def calc_power(report: list, size: int)->int:
+	most_common = ''
+	least_common = ''
+	for i in range(0, size):
+		count = 0
+		for num in report:
+			if num[i] == '1':
+				count+=1
+		if count > len(report)//2:
+			most_common+='1'
+			least_common+='0'
+		else:
+			most_common+='0'
+			least_common+='1'
+	return int(most_common,2) * int(least_common,2)
 
 if __name__ == '__main__':
 	with open(sys.argv[1], 'r') as f, open(sys.argv[2], 'w') as out:
 		content = [line.strip() for line in f]
-		solution = calc_power(content, len(content[0]))
+		solution = calc_power2(content, len(content[0]))
 		out.write(str(solution))
